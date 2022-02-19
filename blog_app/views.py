e566b743174187b7f93f41af03ba72995c4298f2
SaberHardy <samirsamado5@gmail.com>
2022-02-20 00:10:11 +0300

    add marketing(sigUp) model

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
 from blog_app.models import Post
+from marketing.models import Signup
 
 
 def index(request):
     featured = Post.objects.filter(featured=True)
     latest = Post.objects.order_by('-timestamp')[:3]
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+
     context = {
         'object_list': featured,
         'latest': latest,
--- just_blog/settings.py
+++ just_blog/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog_app.apps.BlogAppConfig',
+    'marketing.apps.MarketingConfig',
 ]
 
 MIDDLEWARE = [
