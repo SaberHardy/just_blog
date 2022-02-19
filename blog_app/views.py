c2bc5b7ca088b9aec6e65130c31d8c6e2a7981eb
SaberHardy <samirsamado5@gmail.com>
2022-02-19 19:17:03 +0300

    add some initial views

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog_app/index.html')
+
+
+def post(request):
+    return render(request, 'blog_app/post.html')
+
+
+def blog(request):
+    return render(request, 'blog_app/blog.html')
