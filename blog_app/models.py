a77678ad8935084621e34b80eea86509cc0fc3e5
SaberHardy <samirsamado5@gmail.com>
2022-02-19 22:16:59 +0300

    create models(Post, Category, Author)

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    comment_count = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    image = models.ImageField()
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
