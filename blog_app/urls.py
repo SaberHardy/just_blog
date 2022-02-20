97175311b19941602e16704b4ef5fe841f8c20a1
SaberHardy <samirsamado5@gmail.com>
2022-02-20 22:15:21 +0300

    search function

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('blog/', views.blog, name='post-list'),
+    path('search/', views.search, name='search'),
     path('post/<id>/', views.post, name='post-detail'),
 ]
