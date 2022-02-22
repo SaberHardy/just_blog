38959478d1fb4ec7bcfa2b387db7b9015aa59891
SaberHardy <samirsamado5@gmail.com>
2022-02-22 14:50:37 +0300

    User SignUp Form

--- members/forms.py
+++ members/forms.py
@@ -1,1 +1,38 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+class UserSignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100,
+                                 strip=True,
+                                 widget=forms.TextInput(attrs={
+                                     'class': 'form-control',
+                                     'placeholder': 'Type your first name'}))
+
+    last_name = forms.CharField(max_length=100,
+                                widget=forms.TextInput(attrs={
+                                    'class': 'form-control',
+                                    'placeholder': 'Type your last name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+                                 'class': 'form-control',
+                                 'placeholder': 'Type your email'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserSignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Type your username'
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Type your password'
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Type your confirmation password'
+
+        for field in ['password1', 'username']:
+            self.fields[field].help_text = None
--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registration/', views.UserRegisterView.as_view(), name='register')
+    path('register/', views.UserRegisterView.as_view(), name='register')
 ]
